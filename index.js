df668ba87ffbd347e81e7eda2bd915e40df2947f
method99man <damian1717@gmail.com>
2025-03-18 23:57:21 +0100

    " Initial commit: Base project structure, Added: Basic functionality, Working on: UI. "

---
+++ index.js
@@ -0,0 +1,36 @@
+const buttonContainer = document.querySelector(".buttons-container");
+const mainContainer = document.querySelector(".container");
+let firstNumber, secondNumber, operator;
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+      break;
+    case "-":
+      return subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      return multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      return divide(firstNumber, secondNumber);
+      break;
+  }
+}
+
+console.log(operate(7, "-", 8));
+console.log(buttonContainer);
