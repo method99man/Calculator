7ae197d90f69bf4c0b6f035c3cf4678ac32a157e
method99man <damian1717@gmail.com>
2025-04-08 00:15:37 +0200

    Working on calculator logic

--- index.js
+++ index.js
@@ -1,45 +1,94 @@
-const buttonContainer = document.querySelector(".buttons-container");
-const mainContainer = document.querySelector(".container");
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
-let firstNumber,
-  secondNumber,
-  operator,
-  calculation = [];
+class Calculator {
+  constructor(currentOperandTextElement) {
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
 
-function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
-}
-function subtract(firstNumber, secondNumber) {
-  return firstNumber - secondNumber;
-}
-function multiply(firstNumber, secondNumber) {
-  return firstNumber * secondNumber;
-}
-function divide(firstNumber, secondNumber) {
-  return firstNumber / secondNumber;
-}
+  removeElementsFromArray(arr) {
+    const elementsToRemove = ["+", "-", "*", "/"];
+    return arr.filter((item) => !elementsToRemove.includes(item));
+  }
 
-function operate(firstNumber, operator, secondNumber) {
-  switch (operator) {
-    case "+":
-      return add(firstNumber, secondNumber);
-      break;
-    case "-":
-      return subtract(firstNumber, secondNumber);
-      break;
-    case "*":
-      return multiply(firstNumber, secondNumber);
-      break;
-    case "/":
-      return divide(firstNumber, secondNumber);
-      break;
+  clear() {
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
+
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === 0 || this.currentOperand === "") return;
+    if (this.currentOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.currentOperand += this.operation;
+  }
+
+  compute() {
+    let computation;
+    let listOfNumbers = this.removeElementsFromArray([...this.currentOperand]);
+    console.log(listOfNumbers);
+
+    const current = parseFloat(this.currentOperand);
+
+    if (isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = Number(listOfNumbers[0]) + Number(listOfNumbers[1]);
+        break;
+      case "-":
+        computation = Number(listOfNumbers[0]) - Number(listOfNumbers[1]);
+        break;
+      case "*":
+        computation = Number(listOfNumbers[0]) * Number(listOfNumbers[1]);
+        break;
+      case "/":
+        computation = Number(listOfNumbers[0]) / Number(listOfNumbers[1]);
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
   }
 }
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    calculation.push(btn.textContent);
-    console.log(calculation);
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-delete]");
+const currentOperandTextElement = document.querySelector(".display");
+
+const calculator = new Calculator(currentOperandTextElement);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
   });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+clearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
 });
